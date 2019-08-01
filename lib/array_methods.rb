8209233ae89babed65a4e91a77177d34182e7b7a
Dean Burgess <dinocastiglione@ymail.com>
2019-08-01 15:17:01 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -12,10 +12,10 @@ def find_max_value(array)
   max = array.first
   counter = 1 
   while counter < array.length do
-    # if array[counter] > max
-    #   max = array[counter]
-    # end
-    # counter += 1
+    if array[counter] > max
+      max = array[counter]
+    end
+    counter += 1
   end
   return max
 end
